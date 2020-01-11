769be19c7725d2a0037d567df6c1b1c76cb7296c
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-01-11 19:10:39 -0300

    :bug: use persisted value as initial state if available

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 interface IStorage {
   getItem(key: string): string | null;
@@ -19,20 +19,19 @@ const defaultOptions: Options = {
 export const useStorage = (key: string, options: Options = defaultOptions) => {
   const { storage = window.localStorage, placeholder } = options;
 
+  const [value, setValue] = useState<string>(() => {
+    const persistedValue = storage.getItem(key);
+    if (persistedValue === null) {
+      storage.setItem(key, placeholder);
+      return placeholder;
+    }
+    return persistedValue;
+  });
+
   const setStorageValue = (newValue: string) => {
     setValue(newValue);
     storage.setItem(key, newValue);
   };
-
-  const [value, setValue] = useState<string | null>(placeholder);
-
-  useEffect(() => {
-    if (value) storage.setItem(key, value);
-  }, [value]);
-
-  useEffect(() => {
-    setValue(storage.getItem(key));
-  }, [storage.getItem(key)]);
 
   return [value, setStorageValue] as const;
 };
