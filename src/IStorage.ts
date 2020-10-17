eaec5f456c8b9492c55173991881f7303430ccfa
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-10-17 20:58:46 -0300

    🔥 remove "removeItem" from IStorage interface

--- src/IStorage.ts
+++ src/IStorage.ts
@@ -14,12 +14,6 @@ interface IStorage {
    * @param value - Value to be set
    */
   setItem(key: string, value: string): void;
-
-  /**
-   * Remove the value at `key` passed
-   * @param key - Key used to access the Storage value
-   */
-  removeItem(key: string): void;
 }
 
 export default IStorage;
